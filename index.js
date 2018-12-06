fcd383a570fdfa20f88ee7a0f85aad67d06fc007
Tony Freeman <ttfreeman@gmail.com>
2018-12-06 15:51:48 -0700

    connect serverside and dialogflow attempt1

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
 const express = require("express");
+const structjson = require("./structjson");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send({ message: "hello" });
+  res.send({ message: "johnny" });
+});
+
+app.post("/api/df_text_query", async (req, res) => {
+  // You can find your project ID in your Dialogflow agent settings
+  process.env.GOOGLE_APPLICATION_CREDENTIALS =
+    "./../../../Keys/react-chatbot-fb45c8f8db9c.json";
+  const projectId = "react-chatbot-6d094"; //https://dialogflow.com/docs/agents#settings
+  const sessionId = "react-bot-session";
+
+  const languageCode = "en-US";
+
+  // Instantiate a DialogFlow client.
+  const dialogflow = require("dialogflow");
+  const sessionClient = new dialogflow.SessionsClient();
+
+  // Define session path
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: req.body.text,
+        languageCode: languageCode
+      }
+    },
+    queryParams: {
+      payload: {
+        data: req.body.parameters
+      }
+    }
+  };
+
+  // Send request and log result
+  let responses = await sessionClient.detectIntent(request);
+  res.send(responses[0].queryResult);
+});
+
+app.post("/api/df_event_query", async (req, res) => {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS =
+    "./../../../Keys/react-chatbot-fb45c8f8db9c.json";
+  const projectId = "react-chatbot-6d094";
+  const sessionId = "react-bot-session";
+
+  const languageCode = "en-US";
+
+  const dialogflow = require("dialogflow");
+  const sessionClient = new dialogflow.SessionsClient();
+
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      event: {
+        name: req.body.event,
+        parameters: structjson.jsonToStructProto(req.body.parameters),
+        languageCode: languageCode
+      }
+    }
+  };
+
+  let responses = await sessionClient.detectIntent(request);
+  res.send(responses[0].queryResult);
 });
 
 const PORT = process.env.PORT || 5000;
